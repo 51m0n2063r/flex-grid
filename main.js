255a44440e1433e156bc0a63d8ff108e8eb2e4cc
Valentin Janvier <alencruserxx@hotmail.fr>
2018-05-16 13:13:38 +0200

    prototype box movibles

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-$('flex').click(function(){//Fonction qui marcherai sur un appui de bouton et etendrai la box au max de sa taille.
-	$('#divflex').css('height','200px')
-	$('#divflex').css('width','100%')//Cette ligne permet de donner toute la div en taille a notre espace (a condition d'avoir donné une taille 0 a son opposé avant)
-	$('#divflex').css('justify-content','center')//Exemple de ligne qui permettrai de modifier le flex
-	$('#divflex').css('','')//Sur cette ligne un modèle pur et dur réutilisable pour chaque modif css a ajouter.
+$('bflex').click(function(){//Fonction qui marcherai sur un appui de bouton et etendrai la box au max de sa taille.
+	$('#flex').css('height','200px')
+	$('#flex').css('width','100%')//Cette ligne permet de donner toute la div en taille a notre espace (a condition d'avoir donné une taille 0 a son opposé avant)
+	$('#flex').css('justify-content','center')//Exemple de ligne qui permettrai de modifier le flex
+	$('#flex').css('','')//Sur cette ligne un modèle pur et dur réutilisable pour chaque modif css a ajouter.
 }
