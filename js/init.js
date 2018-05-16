81fc23d8ee6a51c04c9be77002175baec03d4007
51m0n2063r <simon.roger31@gmail.com>
2018-05-16 14:10:49 +0200

    add retouche et fonction

--- js/init.js
+++ js/init.js
@@ -3,14 +3,12 @@
 
     $('.sidenav').sidenav();
       
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });     
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 $(document).ready(function(){
     $('.parallax').parallax();
+    $('.materialboxed').materialbox();
   });
+
--- main.js
+++ main.js
@@ -3,4 +3,4 @@ $('flex').click(function(){//Fonction qui marcherai sur un appui de bouton et et
 	$('#divflex').css('width','100%')//Cette ligne permet de donner toute la div en taille a notre espace (a condition d'avoir donné une taille 0 a son opposé avant)
 	$('#divflex').css('justify-content','center')//Exemple de ligne qui permettrai de modifier le flex
 	$('#divflex').css('','')//Sur cette ligne un modèle pur et dur réutilisable pour chaque modif css a ajouter.
-}
+});
