6afe3756a75fa8367d5aa53ffa2d44f315640b32
51m0n2063r <simon.roger31@gmail.com>
2018-05-16 13:49:04 +0200

    add img grid et flex et sources

--- js/init.js
+++ js/init.js
@@ -2,9 +2,15 @@
   $(function(){
 
     $('.sidenav').sidenav();
+      
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });     
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
 $(document).ready(function(){
     $('.parallax').parallax();
   });
