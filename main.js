14c3e3af1a7632b86e844cd7cdac79aa0b9e1ce6
Alencruser <38475565+Alencruser@users.noreply.github.com>
2018-05-16 14:28:34 +0200

    Focus on grid part : start

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ $('bflex').click(function(){//Fonction qui marcherai sur un appui de bouton et e
 	$('#flex').css('width','100%')//Cette ligne permet de donner toute la div en taille a notre espace (a condition d'avoir donné une taille 0 a son opposé avant)
 	$('#flex').css('justify-content','center')//Exemple de ligne qui permettrai de modifier le flex
 	$('#flex').css('','')//Sur cette ligne un modèle pur et dur réutilisable pour chaque modif css a ajouter.
-}
+})
