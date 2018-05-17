4dc4f3d33df73ceca8128e88c14899a2d9b4e964
Valentin Janvier <alencruserxx@hotmail.fr>
2018-05-17 22:19:52 +0200

    Ajout du travail Js coté grid sur le repo natif

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-$('bflex').click(function(){//Fonction qui marcherai sur un appui de bouton et etendrai la box au max de sa taille.
-	$('#flex').css('height','200px')
-	$('#flex').css('width','100%')//Cette ligne permet de donner toute la div en taille a notre espace (a condition d'avoir donné une taille 0 a son opposé avant)
-	$('#flex').css('justify-content','center')//Exemple de ligne qui permettrai de modifier le flex
-	$('#flex').css('','')//Sur cette ligne un modèle pur et dur réutilisable pour chaque modif css a ajouter.
-}
+var p="Le cube";
+$('#bugrid').click(function(){//Fonction qui marcherai sur un appui de bouton et etendrai la box au max de sa taille.
+	$('#flex').css('display','none');
+	$('#grid').css('width','60%');//Cette ligne permet de donner toute la div en taille a notre espace (a condition d'avoir donné une taille 0 a son opposé avant)
+	$('#exgrid').css('display','inline');//Exemple de ligne qui permettrai de modifier le flex
+	$('#grid').css("border-radius","10px");
+})
+$('#reini').click(function(){
+	$('#exgrid').css('display','');
+	$('#flex').css('display','');
+	$('#grid').css('width','50%');
+	$('#grid').css('border-radius','');
+	$('#helpgrid').css('display','none')
+})
+$('#grid-row').click(function(){
+	$('#helpgrid').css('display','inline');
+	$('#exgrid').css('display','none');
+})
+$('#helpgrid').dblclick(function(){
+	$('#exgrid').css('display','inline');
+	$('#helpgrid').css('display','none');
+})
