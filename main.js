56450de6b3052b9975daa3a5cff9ac25b13b6321
Valentin Janvier <alencruserxx@hotmail.fr>
2018-05-18 10:38:28 +0200

    save avant pull

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ $('#bugrid').click(function(){//Fonction qui marcherai sur un appui de bouton et
 	$('#flex').css('display','none');
 	$('#grid').css('width','60%');//Cette ligne permet de donner toute la div en taille a notre espace (a condition d'avoir donné une taille 0 a son opposé avant)
 	$('#exgrid').css('display','inline');//Exemple de ligne qui permettrai de modifier le flex
-	$('#grid').css("border-radius","10px");
 })
 $('#reini').click(function(){
 	$('#exgrid').css('display','');
 	$('#flex').css('display','');
 	$('#grid').css('width','50%');
-	$('#grid').css('border-radius','');
-	$('#helpgrid').css('display','none')
+	$('#helpgrid').css('display','none');
+	$('#cb1').css('grid-row','1');
+	$('#cb3').css('grid-row','1');
+	$('#cb4').css('grid-row','3');
+	$('#css').replaceWith('<link id="css" href="css/styles.css">')
 })
 $('#grid-row').click(function(){
 	$('#helpgrid').css('display','inline');
@@ -24,7 +26,4 @@ $('#grid-row').click(function(){
 $('#helpgrid').dblclick(function(){
 	$('#exgrid').css('display','inline');
 	$('#helpgrid').css('display','none');
-	$('#cb1').css('grid-row','1');
-	$('#cb3').css('grid-row','1');
-	$('#cb4').css('grid-row','3');
 })
